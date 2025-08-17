cf6d4cf7c26fce6e7824b0ee40fe8ddbcbf6964f
leucinemymind <emilyy2009x@gmail.com>
2025-08-17 09:16:00 -0700

    fixed

--- diffs.R
+++ diffs.R
@@ -27,15 +27,25 @@ contacts_3O4O <- read.table(
 colnames(contacts_1ITB) <- c("frames", "interaction_type", "atom1", "atom2")
 colnames(contacts_3O4O) <- c("frames", "interaction_type", "atom1", "atom2")
 
-c1ITB_sel <- contacts_1ITB %>% select(interaction_type, atom1, atom2)
-c3O4O_sel <- contacts_3O4O %>% select(interaction_type, atom1, atom2)
+c1ITB_sel <- contacts_1ITB %>% select(interaction_type, atom1, atom2) %>% filter(interaction_type != "vdw")
+c3O4O_sel <- contacts_3O4O %>% select(interaction_type, atom1, atom2) %>% filter(interaction_type != "vdw")
 
-# full join by interaction type to comp
-compare_contacts <- full_join(c1ITB_sel, c3O4O_sel, by = "interaction_type")
+# unique to 1ITB (by atom pairs only)
+unique_1ITB <- anti_join(
+  c1ITB_sel, c3O4O_sel, by = c("atom1", "atom2")
+)
 
-# unique contacts
-unique_contacts <- compare_contacts %>%
-  filter(contacts_1ITB$atom1 != contacts_3O4O$atom1 | contacts_1ITB$atom2 != contacts_3O4O$atom2 | interaction_type != "vdw")
+# unique to 3O4O (by atom pairs only)
+unique_3O4O <- anti_join(
+  c3O4O_sel, c1ITB_sel, by = c("atom1", "atom2")
+)
+
+unique_contacts <- bind_rows(
+  unique_1ITB %>% mutate(source = "1ITB") %>%
+    filter(interaction_type != "vdw"),
+  unique_3O4O %>% mutate(source = "3O4O") %>%
+    filter(interaction_type != "vdw")
+)
 
 write.csv(unique_contacts, 
           "~/Dropbox/getcontacts/receptors_il1b/unique_contacts.csv", 
