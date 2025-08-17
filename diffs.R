0700eca476b38793506f41b2d58e1ee087e6dd8d
leucinemymind <emilyy2009x@gmail.com>
2025-08-16 21:21:22 -0700

    R

---
+++ diffs.R
@@ -0,0 +1,6 @@
+### Finding differences between contacts ###
+### 2025-08-16 21:02 PT ###
+
+library(dplyr)
+
+contacts_1ITB <- read.delim("~/Dropbox/getcontacts/receptors_il1b/1itb_contacts.tsv", header = FALSE, fill = TRUE, stringsAsFactors = FALSE)
