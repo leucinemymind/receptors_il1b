0831050183470e30ac5014b9f18d00de2d9c6b6f
leucinemymind <emilyy2009x@gmail.com>
2025-08-17 22:39:44 -0700

    ??????

--- diffs.R
+++ diffs.R
@@ -7,11 +7,11 @@ library(dplyr)
 
 # reading tsv files
 contacts_1ITB <- read.table(
-"~/Dropbox/getcontacts/receptors_il1b/1itb_contacts.tsv", 
-skip = 2, 
-header = FALSE, 
-fill = TRUE, 
-stringsAsFactors = FALSE
+  "~/Dropbox/getcontacts/receptors_il1b/1itb_contacts.tsv", 
+  skip = 2, 
+  header = FALSE, 
+  fill = TRUE, 
+  stringsAsFactors = FALSE
 )
 
 contacts_3O4O <- read.table(
@@ -27,8 +27,8 @@ contacts_3O4O <- read.table(
 colnames(contacts_1ITB) <- c("frames", "interaction_type", "atom1", "atom2")
 colnames(contacts_3O4O) <- c("frames", "interaction_type", "atom1", "atom2")
 
-c1ITB_sel <- contacts_1ITB %>% select(interaction_type, atom1, atom2)
-c3O4O_sel <- contacts_3O4O %>% select(interaction_type, atom1, atom2)
+c1ITB_sel <- contacts_1ITB %>% select(interaction_type, atom1, atom2) %>% filter(interaction_type != "vdw")
+c3O4O_sel <- contacts_3O4O %>% select(interaction_type, atom1, atom2) %>% filter(interaction_type != "vdw")
 
 # unique to 1ITB (by atom pairs only)
 unique_1ITB <- anti_join(
