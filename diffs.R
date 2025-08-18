a514186abb56a20c372ae6a0bbf8ed12d2a570aa
leucinemymind <emilyy2009x@gmail.com>
2025-08-18 09:33:44 -0700

    rewrote code

--- diffs.R
+++ diffs.R
@@ -1,52 +1,38 @@
-### Finding differences between contacts ###
-### 2025-08-16 21:02 PT ###
-
 # packages
 
-library(dplyr)
+library(tidyverse)
 
-# reading tsv files
-contacts_1ITB <- read.table(
-  "~/Dropbox/getcontacts/receptors_il1b/1itb_contacts.tsv", 
-  skip = 2, 
-  header = FALSE, 
-  fill = TRUE, 
-  stringsAsFactors = FALSE
-)
+# load data
 
-contacts_3O4O <- read.table(
-  "~/Dropbox/getcontacts/receptors_il1b/3o4o_contacts.tsv", 
-  skip = 2, 
-  header = FALSE, 
-  fill = TRUE, 
-  stringsAsFactors = FALSE
-)
+contacts_1ITB <- read_tsv("~/Dropbox/getcontacts/receptors_il1b/1itb_contacts.tsv", skip = 2)
+contacts_3O4O <- read_tsv("~/Dropbox/getcontacts/receptors_il1b/3o4o_contacts.tsv", skip = 2)
 
-# column names and selections
+# clean up column names
 
 colnames(contacts_1ITB) <- c("frames", "interaction_type", "atom1", "atom2")
 colnames(contacts_3O4O) <- c("frames", "interaction_type", "atom1", "atom2")
 
-c1ITB_sel <- contacts_1ITB %>% select(interaction_type, atom1, atom2) %>% filter(interaction_type != "vdw")
-c3O4O_sel <- contacts_3O4O %>% select(interaction_type, atom1, atom2) %>% filter(interaction_type != "vdw")
+c1ITB_sel <- contacts_1ITB %>% select(-frames)
+c3O4O_sel <- contacts_3O4O %>% select(-frames)
 
-# unique to 1ITB (by atom pairs only)
-unique_1ITB <- anti_join(
-  c1ITB_sel, c3O4O_sel, by = c("atom1", "atom2")
+# find unique contacts
+
+unique_contacts_c1ITB <- c1ITB_sel %>%
+  anti_join(c3O4O_sel, by = c("atom1", "atom2")) %>%
+  rename(atom1_c1ITB = atom1, atom2_c1ITB = atom2) %>%
+  mutate(interaction = paste(atom1_c1ITB, atom2_c1ITB, sep = "-")) %>%
+  select(interaction, interaction_type) %>% filter(interaction_type != "vdw")
+
+unique_contacts_c3O4O <- c3O4O_sel %>%
+  anti_join(c1ITB_sel, by = c("atom1", "atom2")) %>%
+  rename(atom1_c3O4O = atom1, atom2_c3O4O = atom2) %>%
+  mutate(interaction = paste(atom1_c3O4O, atom2_c3O4O, sep = "-")) %>%
+  select(interaction, interaction_type) %>% filter(interaction_type != "vdw")
+
+write_csv(unique_contacts_c1ITB, 
+          "~/Dropbox/getcontacts/receptors_il1b/unique_contacts_1ITB.csv"
 )
 
-# unique to 3O4O (by atom pairs only)
-unique_3O4O <- anti_join(
-  c3O4O_sel, c1ITB_sel, by = c("atom1", "atom2")
+write_csv(unique_contacts_c3O4O, 
+          "~/Dropbox/getcontacts/receptors_il1b/unique_contacts_3O4O.csv"
 )
-
-unique_contacts <- bind_rows(
-  unique_1ITB %>% mutate(source = "1ITB") %>%
-    filter(interaction_type != "vdw"),
-  unique_3O4O %>% mutate(source = "3O4O") %>%
-    filter(interaction_type != "vdw")
-)
-
-write.csv(unique_contacts, 
-          "~/Dropbox/getcontacts/receptors_il1b/unique_contacts.csv", 
-          row.names = FALSE)
--- diffs_test.R
+++ diffs_test.R
@@ -1,8 +1,13 @@
 # packages
+
 library(tidyverse)
+
+# load data
 
 contacts_1ITB <- read_tsv("~/Dropbox/getcontacts/receptors_il1b/1itb_contacts.tsv", skip = 2)
 contacts_3O4O <- read_tsv("~/Dropbox/getcontacts/receptors_il1b/3o4o_contacts.tsv", skip = 2)
+
+# clean up column names
 
 colnames(contacts_1ITB) <- c("frames", "interaction_type", "atom1", "atom2")
 colnames(contacts_3O4O) <- c("frames", "interaction_type", "atom1", "atom2")
@@ -10,13 +15,16 @@ colnames(contacts_3O4O) <- c("frames", "interaction_type", "atom1", "atom2")
 c1ITB_sel <- contacts_1ITB %>% select(-frames)
 c3O4O_sel <- contacts_3O4O %>% select(-frames)
 
-# unique contacts
-unique_contacts <- c1ITB_sel %>%
+# find unique contacts
+
+unique_contacts_c1ITB <- c1ITB_sel %>%
   anti_join(c3O4O_sel, by = c("atom1", "atom2")) %>%
   rename(atom1_c1ITB = atom1, atom2_c1ITB = atom2) %>%
   mutate(interaction = paste(atom1_c1ITB, atom2_c1ITB, sep = "-")) %>%
-  select(interaction, atom1_c1ITB, atom2_c1ITB)
+  select(interaction, interaction_type) %>% filter(interaction_type != "vdw")
 
-unique_contacts <- unique_contacts %>%
-  left_join(c3O4O_sel %>% rename(atom1_c3O4O = atom1, atom2_c3O4O = atom2),
-            by = c("atom1_c1ITB" = "atom1_c3O4O", "atom2_c1ITB" = "atom2_c3O4O")) 
+unique_contacts_3O4O <- c3O4O_sel %>%
+  anti_join(c1ITB_sel, by = c("atom1", "atom2")) %>%
+  rename(atom1_c3O4O = atom1, atom2_c3O4O = atom2) %>%
+  mutate(interaction = paste(atom1_c3O4O, atom2_c3O4O, sep = "-")) %>%
+  select(interaction, interaction_type) %>% filter(interaction_type != "vdw")
