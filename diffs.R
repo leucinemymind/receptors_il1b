b05c5b997ccecc25d97c64706b0abbf8af360bdd
leucinemymind <emilyy2009x@gmail.com>
2025-08-24 11:11:10 -0700

    got frequency file

--- diffs.R
+++ diffs.R
@@ -1,3 +1,6 @@
+## Finding differences between contacts ##
+## 2025-08-16 21:02 ##
+
 # packages
 
 library(tidyverse)
@@ -21,18 +24,18 @@ unique_contacts_c1ITB <- c1ITB_sel %>%
   anti_join(c3O4O_sel, by = c("atom1", "atom2")) %>%
   rename(atom1_c1ITB = atom1, atom2_c1ITB = atom2) %>%
   mutate(interaction = paste(atom1_c1ITB, atom2_c1ITB, sep = "-")) %>%
-  select(interaction, interaction_type) %>% filter(interaction_type != "vdw")
+  select(interaction, interaction_type)
 
 unique_contacts_c3O4O <- c3O4O_sel %>%
   anti_join(c1ITB_sel, by = c("atom1", "atom2")) %>%
   rename(atom1_c3O4O = atom1, atom2_c3O4O = atom2) %>%
   mutate(interaction = paste(atom1_c3O4O, atom2_c3O4O, sep = "-")) %>%
-  select(interaction, interaction_type) %>% filter(interaction_type != "vdw")
+  select(interaction, interaction_type)
 
-write_csv(unique_contacts_c1ITB, 
+write.csv(unique_contacts_c1ITB, 
           "~/Dropbox/getcontacts/receptors_il1b/unique_contacts_1ITB.csv"
 )
 
-write_csv(unique_contacts_c3O4O, 
+write.csv(unique_contacts_c3O4O, 
           "~/Dropbox/getcontacts/receptors_il1b/unique_contacts_3O4O.csv"
 )
