4244f1fe17cfdb3d81a67b8a4a3a6983e4d0881d
leucinemymind <emilyy2009x@gmail.com>
2025-12-23 20:23:51 -0800

    boxplot

--- autodock/to_do/analysis.R
+++ autodock/to_do/analysis.R
@@ -1,5 +1,14 @@
 #packages + data
-library(tidyverse)
-inhibition_data <- read_csv("~/Downloads/il1b_inhibition_data - Sheet1.csv")
 
-t.test(inhibition_data$affinity_1ITB, inhibition_data$affinity_3O4O, paired = TRUE)
+# t-test
+library(tidyverse)
+inhibition_data_tidy <- read_csv("~/Dropbox/getcontacts/receptors_il1b/autodock/il1b_inhibition_data - Sheet1 (1).csv")
+inhibition_data_normal <- read_csv("~/Dropbox/getcontacts/receptors_il1b/autodock/il1b_inhibition_data - Sheet1 (2).csv")
+
+ggplot(inhibition_data, aes(x = protein, y = affinity)) +
+  geom_boxplot(fill = "skyblue") +
+  geom_jitter(width = 0.1, alpha = 0.5) +
+  labs(title = "Docking Affinities", x = "protein", y = "affinity (kcal/mol)") +
+  theme_minimal()
+
+t.test(inhibition_data_normal$affinity_1ITB, inhibition_data_normal$affinity_3O4O, paired = TRUE)
